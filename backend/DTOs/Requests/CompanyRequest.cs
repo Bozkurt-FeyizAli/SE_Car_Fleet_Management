4cd479f72e695bcff58293a54d9fd8b4b1190c9b
Bozkurt-FeyizAli <Feyizalibozkurt@gmail.com>
2026-04-01 15:42:27 +0300

    requests

---
+++ backend/DTOs/Requests/CompanyRequest.cs
@@ -0,0 +1,8 @@
+namespace Backend.DTOs
+{
+    public class CompanyRequest
+    {
+        public string CompanyName { get; set; } = string.Empty;
+        public string TaxNumber { get; set; } = string.Empty;
+    }
+}
