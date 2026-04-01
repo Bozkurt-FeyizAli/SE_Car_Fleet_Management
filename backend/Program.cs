ed49cd00c7851294fc7e791eac7ba9fd90489b73
Bozkurt-FeyizAli <Feyizalibozkurt@gmail.com>
2026-04-01 15:41:42 +0300

    necessary sys files

---
+++ backend/Program.cs
@@ -0,0 +1,47 @@
+using Backend.Data;
+using Backend.Services;
+using Backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IVehicleService, VehicleService>();
+builder.Services.AddScoped<IRentalService, RentalService>();
+builder.Services.AddScoped<IManagerService, ManagerService>();
+builder.Services.AddScoped<ICompanyService, CompanyService>();
+builder.Services.AddScoped<IVehicleRegistrationService, VehicleRegistrationService>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+var app = builder.Build();
+
+// Veritabanını otomatik olarak tablo yapılarına göre sıfırla ve yeniden oluştur (Geliştirme Ortamı İçin)
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    // context.Database.EnsureDeleted(); // Geliştirme aşamasında veritabanı şeması oturduğu için kapattık.
+    // context.Database.EnsureCreated(); // Veri kaybını önlemek için kapattık.
+}
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
